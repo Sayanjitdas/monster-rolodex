99da61ca62acdc836c6afb4230051311e4d96cc5
sayanjitdas <sayanjitgit@gmail.com>
2019-12-18 18:04:09 +0530

    Updates

---
+++ precache-manifest.0ec38673a58b373fd2956dcf2a86e289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71131ca2eef634624d03c09db6f0706",
+    "url": "/Sayanjitdas/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0335b6350feb58b69245",
+    "url": "/Sayanjitdas/monster-rolodex/static/css/main.983cf986.chunk.css"
+  },
+  {
+    "revision": "84670d56f232dc259ddf",
+    "url": "/Sayanjitdas/monster-rolodex/static/js/2.ede2814f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Sayanjitdas/monster-rolodex/static/js/2.ede2814f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0335b6350feb58b69245",
+    "url": "/Sayanjitdas/monster-rolodex/static/js/main.6a7aa65a.chunk.js"
+  },
+  {
+    "revision": "91f3b9f68d80a2c65589",
+    "url": "/Sayanjitdas/monster-rolodex/static/js/runtime-main.d4d68940.js"
+  }
+]);
